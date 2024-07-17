d9c2f4cca7f9370e351d80c9e823c3b9bc917e72
Choster900 <16adonaysergio@gmail.com>
2024-07-17 09:20:42 -0600

    change: changing route for connection in API

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-//export const API_URL = 'http://localhost:8080/v1';
-//export const IMAGE_PATH = 'http://localhost:8080/';
-export const API_URL = 'https://cursify-rest-production.up.railway.app/v1';
-export const IMAGE_PATH = 'https://cursify-rest-production.up.railway.app/';
+export const API_URL = 'http://localhost:8080/v1';
+export const IMAGE_PATH = 'http://localhost:8080/';
+//export const API_URL = 'https://cursify-rest-production.up.railway.app/v1';
+//export const IMAGE_PATH = 'https://cursify-rest-production.up.railway.app/';
